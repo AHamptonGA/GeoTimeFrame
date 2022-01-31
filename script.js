60f123be29dd6e4f91088f1415406e6fbaf4f9c7
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-01-31 14:01:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,14 +18,22 @@ let time = datetime.toLocaleString('en-UK', { timeZone: tz, hour: '2-digit', min
 function get_popup_div(feature) {
   
 	const timeZoneId = feature.graphic.attributes.tzid;
-	const localDt = get_current_tz_conv(timeZoneId);
+	const localDt = get_current_date_conv(timeZoneId);
 	const localTm = get_current_tz_conv(timeZoneId);
+	
+	const utcDt = get_current_date_conv('UTC');
+	const utcTm = get_current_tz_conv('UTC');
+	
 	const div = document.createElement("div");
 	div.innerHTML =
 					`
 					<b>INNA ID: </b> ${timeZoneId}<br> 
 					<b>LOCAL DATE: </b> ${localDt}<br>
 					<b>LOCAL TIME: </b> ${localTm}<br>
+					<br>
+					<b>UTC:</b><br>
+					<b>DATE: </b> ${utcDt}<br>
+					<b>TIME: </b> ${utcTm}<br>
 					`;
   return div;
 };
