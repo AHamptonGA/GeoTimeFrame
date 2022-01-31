fa62a7264474ca02f3b4cc44be1fd4d478a39b05
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-01-31 07:23:40 -0600

    new call to get_current_tz_conv

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 async function get_current_tz_conv(timeZoneId) {
-	
-	let datetime = new Date();
-	
-	let localTZ = datetime.toLocaleDateString('en-US', 
+	const datetime = new Date();
+	const localTZ = datetime.toLocaleDateString('en-US', 
 							{timeZone: timeZoneId,});
 	return localTZ
 }
+
+
+function get_current_tz_conv(feature) {
+  
+	const timeZoneId = feature.graphic.attributes.tzid;
+	const local_tm = await get_current_tz_conv(timeZoneId);
+	
+	div.innerHTML =
+					`
+					<b>INNA ID: </b> {tzid}<br> 
+					<b>LOCAL: </b> {local_tm}<br>
+					`;
+  return div;
+};
