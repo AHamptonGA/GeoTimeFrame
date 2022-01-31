7ff18541d1ca0e815c7404cf9235360d5c28ac72
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-01-31 13:52:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-function get_current_tz_conv(timeZoneId) {
+function get_current_date_conv(timeZoneId) {
 	const datetime = new Date();
-	const localTZ = datetime.toLocaleDateString('en-US', 
+	const localDt = datetime.toLocaleDateString('en-US', 
 							{timeZone: timeZoneId,});
-	return localTZ
+	return localDt
 }
 
+function get_current_tz_conv(timeZoneId) {
+	const datetime = new Date();
+	const localTm = datetime.toLocaleString('en-UK', 
+			{timeZone: tz, hour: '2-digit', minute:'2-digit'});
+	return localTm
+}
+
+
+let time = datetime.toLocaleString('en-UK', { timeZone: tz, hour: '2-digit', minute:'2-digit' });
 
 function get_popup_div(feature) {
   
 	const timeZoneId = feature.graphic.attributes.tzid;
-	const local_tm = get_current_tz_conv(timeZoneId);
+	const localDt = get_current_tz_conv(timeZoneId);
+	const localTm = get_current_tz_conv(timeZoneId);
 	const div = document.createElement("div");
 	div.innerHTML =
 					`
 					<b>INNA ID: </b> ${timeZoneId}<br> 
-					<b>LOCAL: </b> ${local_tm}<br>
+					<b>LOCAL DATE: </b> ${localDt}<br>
+					<b>LOCAL TIME: </b> ${localTm}<br>
 					`;
   return div;
 };
