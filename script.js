3fba998f72e835bb5134ac4a1fdf3edd515d5ac2
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-01-31 15:02:42 -0600

    js fix2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function get_current_date_conv(timeZoneId) {
 function get_current_tz_conv(timeZoneId) {
 	const datetime = new Date();
 	const localTm = datetime.toLocaleString('en-UK', 
-			{timeZone: tz, hour: '2-digit', minute:'2-digit'});
+			{timeZone: timeZoneId, hour: '2-digit', minute:'2-digit'});
 	return localTm
 }
 
@@ -19,6 +19,8 @@ function get_popup_div(feature) {
 	const localDt = get_current_date_conv(timeZoneId);
 	const localTm = get_current_tz_conv(timeZoneId);
 	
+	const utcDt = get_current_date_conv('UTC');
+	const utcTm = get_current_tz_conv('UTC');
 	
 	const div = document.createElement("div");
 	div.innerHTML =
@@ -26,7 +28,10 @@ function get_popup_div(feature) {
 					<b>INNA ID: </b> ${timeZoneId}<br> 
 					<b>LOCAL DATE: </b> ${localDt}<br>
 					<b>LOCAL TIME: </b> ${localTm}<br>
-
+					<br>
+					<b>UTC:</b><br>
+					<b>DATE: </b> ${utcDt}<br>
+					<b>TIME: </b> ${utcTm}<br>
 					`;
   return div;
 };
