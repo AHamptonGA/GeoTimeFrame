21d5c6997c22c770f1772358bedd8a229a2510d0
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-02-02 05:55:35 -0600

    point test

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-
-
-
 function get_ref_time_zones() {
-	const refTimeZones = ['America/New_York', 'America/Chicago', 
-						  'America/Denver', 'America/Los_Angeles'];
-	return refTimeZones
-}	  
+	const refTimeZones = ['America/New_York', 'America/Chicago',
+		'America/Denver', 'America/Los_Angeles'
+	];
+	return refTimeZones;
+}
 
 function get_current_date_conv(timeZoneId) {
 	const datetime = new Date();
 	const localDt = datetime.toLocaleDateString('en-US', {
 		timeZone: timeZoneId,
 	});
-	return localDt
+	return localDt;
 }
 
 function get_current_tz_conv(timeZoneId) {
@@ -22,7 +20,7 @@ function get_current_tz_conv(timeZoneId) {
 		hour: '2-digit',
 		minute: '2-digit'
 	});
-	return localTm
+	return localTm;
 }
 
 function get_tz_name(timeZoneId) {
@@ -31,7 +29,7 @@ function get_tz_name(timeZoneId) {
 		timeZone: timeZoneId,
 		timeZoneName: 'long'
 	})).match(/[0-9]{2}\s{1}(\D+)/)[1];
-	return tz_full_name
+	return tz_full_name;
 }
 
 function reduce_time_zones(timeZones) {
@@ -40,7 +38,7 @@ function reduce_time_zones(timeZones) {
 		let tz_name = get_tz_name(timeZoneId);
 		tz_dict[tz_name] = timeZoneId;
 	}
-	return Object.values(tz_dict)
+	return Object.values(tz_dict);
 }
 
 function build_popup_html(timeZones) {
@@ -51,13 +49,13 @@ function build_popup_html(timeZones) {
 		let tzName = get_tz_name(timeZoneId);
 		let tzDate = get_current_date_conv(timeZoneId);
 		let tzTime = get_current_tz_conv(timeZoneId);
-		
-		if (timeZones.indexOf(timeZoneId) === 1){
-          refTzStr = `<span class="puRefTz"><em>Reference Time Zones:</em></span><br>`;
+
+		if (timeZones.indexOf(timeZoneId) === 1) {
+			let refTzStr = `<span class="puRefTz"><em>Reference Time Zones:</em></span><br>`;
 		} else {
-			refTzStr = '';
+			let refTzStr = '';
 		}
-		
+
 		outHtml += `${refTzStr}
 					<span class="puTzName"><em>&ensp;${tzName}:</em></span><br>
 					<span class="puDate"><em>&emsp;-DATE:</em> ${tzDate}</span><br>
@@ -65,50 +63,29 @@ function build_popup_html(timeZones) {
 					<br>
 					`;
 	}
-	return outHtml
+	return outHtml;
 }
 
-/* function create_point_graphic(lon, lat) {
-	const point = {
-				  type: "point", 
-				  longitude: lon,
-				  latitude: lat
-				};
-	
-	// Create a symbol for drawing the point
-	const markerSymbol = {
-	  type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-	  color: [226, 119, 40],
-	  outline: {
-		// autocasts as new SimpleLineSymbol()
-		color: [255, 255, 255],
-		width: 2
-	  }
-	};
 
-	// Create a graphic and add the geometry and symbol to it
-	const pointGraphic = new Graphic({
-	  geometry: point,
-	  symbol: markerSymbol
-	});
-		
-	return point
-	} */
-		
 function get_popup_div(feature) {
 
 	const timeZoneId = feature.graphic.attributes.tzid;
 	let lat = feature.graphic.geometry.centroid.latitude;
 	let lon = feature.graphic.geometry.centroid.longitude;
-	//let pnt = create_point_graphic(lon, lat);
-	
+	create_point_graphic(lon, lat);
+	let point = new Point([lon,lat],
+		new SpatialReference(
+			{wkid:(feature.graphic.geometry.centroid.spatialReference.wkid)
+			})
+		);
+
 	let tzArray = [timeZoneId, 'UTC'];
 	let reducedTz = reduce_time_zones(get_ref_time_zones()).filter(x => !tzArray.includes(x));
 	tzArray = tzArray.concat(reducedTz);
 
 	const div = document.createElement("div");
 	div.innerHTML = build_popup_html(tzArray);
-	return div
+	return div;
 }
 
 // // The latitude/longitude string to be converted
@@ -116,8 +93,8 @@ function get_popup_div(feature) {
 
 // // Load dependencies of "coordinateFormatter" module
 // coordinateFormatter.load().then(function() {
-  // // Once the promise returned by load() is resolved,
-  // // use the fromLatitudeLongitude() method to convert
-  // // latitude/longitude string to a point geometry.
-  // let point = coordinateFormatter.fromLatitudeLongitude(latlon);
+// // Once the promise returned by load() is resolved,
+// // use the fromLatitudeLongitude() method to convert
+// // latitude/longitude string to a point geometry.
+// let point = coordinateFormatter.fromLatitudeLongitude(latlon);
 // });
