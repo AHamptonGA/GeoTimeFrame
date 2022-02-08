ab347d682ad4e036c5301705197a5457be83fbc7
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-02-08 09:18:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -172,13 +172,13 @@ require([
 		// to the ScreenPoint definition.
 		view.hitTest(event).then(function (response) {
 			if (response.results.length) {
-				let graphic = response.results.filter(function (result) {
+				let SelGraphic = response.results.filter(function (result) {
 				// check if the graphic belongs to the layer of interest
 				return result.graphic.layer === tzGeojsonLayer;
 				})[0].graphic;
 
 				// do something with the result graphic
-				console.log(graphic.attributes.tzid);
+				console.log(SelGraphic.attributes.tzid);
 			}
   		});
 		//feature.graphic.attributes.tzid;
