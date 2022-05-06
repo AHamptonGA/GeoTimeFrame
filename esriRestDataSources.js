58394759f315a4919f48ed72aea8a31a9d589818
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-05-06 16:00:21 -0500

    Update esriRestDataSources.js

--- esriRestDataSources.js
+++ esriRestDataSources.js
@@ -1,5 +1,6 @@
-var wdcName 		= 'esri_rest_data_sources'; 
-var tableSchema		= {};	
+var wdcName 	= 'esri_rest_data_sources';
+var tableSchema = {};
+var url = 		= '';
 
 async function rest_request(prepedUrl) {
 	try {
@@ -21,8 +22,8 @@ async function rest_request(prepedUrl) {
 async function profile_rest() {
 	var url = document.getElementById('restInput');
 	tableSchema['alias'] = `ESRI Rest Data Sources for: ${url}`;
-	// set types of services to query
-	var service_types = ['MapServer', 'FeatureServer'];
+	tableSchema['description'] = `ESRI Rest Web Data Connector (WDC) to gather data dources from: ${url}`;
+	// set types of services to query	var service_types = ['MapServer', 'FeatureServer'];
 	var tableArray = [];
 
 	async function parse_responses(url, folder) {
@@ -92,83 +93,82 @@ async function profile_rest() {
 			await parse_responses(url, fldr);
 		}
 	}
-	
-	return(tableArray)
+
+	return (tableArray)
 }
 
-								
-(async function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
 
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
-            id: 'url',
+(async function() {
+	// Create the connector object
+	var myConnector = tableau.makeConnector();
+
+	// Define the schema
+	myConnector.getSchema = function(schemaCallback) {
+		var cols = [{
+			id: 'url',
 			alias: 'REST API',
 			description: 'ESRI REST API URL',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: 'directory',
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'directory',
 			alias: 'Directory',
 			description: 'Directory or folder within an ESRI REST API',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-			id: 'service',
-            alias: "Service",
-			description: 'Service within an ESRI REST API',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-			id: 'servicetype',		
-            alias: "Service_Type",
-			description: 'Type of a ESRI REST service (Ex. Map, Feature, Geocode... etc)',
-            dataType: tableau.dataTypeEnum.string
+			dataType: tableau.dataTypeEnum.string
 		}, {
-			id: 'datasettype',		
-            alias: "Dataset_Type",
+			id: 'service',
+			alias: "Service",
+			description: 'Service within an ESRI REST API',
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'servicetype',
+			alias: "Service_Type",
+			description: 'Type of a ESRI REST service (Ex. Map, Feature, Geocode... etc)',
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'datasettype',
+			alias: "Dataset_Type",
 			description: 'Dataset type (Ex. table or geospatial layer)',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-			id: 'dataset',		
-            alias: "Dataset_Name",
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'dataset',
+			alias: "Dataset_Name",
 			description: 'Dataset name',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-			id: 'datasetid',		
-            alias: "Dataset_ID",
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'datasetid',
+			alias: "Dataset_ID",
 			description: 'ESRI REST Dataset ID which is unique within a service',
-            dataType: tableau.dataTypeEnum.string
-        }, {
-			id: 'dataseturl',		
-            alias: "Dataset_URL",
-			description: 'FUll URL to a dataset endpoint on the REST server',
-            dataType: tableau.dataTypeEnum.string			
-        }];
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: 'dataseturl',
+			alias: "Dataset_URL",
+			description: 'Full URL to a dataset endpoint on the REST server',
+			dataType: tableau.dataTypeEnum.string
+		}];
 
-        tableSchema = {
-            id: wdcName,
-            alias: "ESRI Rest Data Sources",
-            columns: cols
-        };
+		tableSchema = {
+			id: wdcName,
+			alias: "ESRI Rest Data Sources",
+			columns: cols
+		};
 
-        schemaCallback([tableSchema]);
-    };
+		schemaCallback([tableSchema]);
+	};
 
-    // Download the data
-    myConnector.getData = async function(table, doneCallback) {
-        tableData = await profile_rest();
+	// Download the data
+	myConnector.getData = async function(table, doneCallback) {
+		tableData = await profile_rest();
 		table.appendRows(tableData);
 		doneCallback();
-		
-    };
+	};
 
-    tableau.registerConnector(myConnector);
+	tableau.registerConnector(myConnector);
 
-    // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = wdcName; 
-            tableau.submit(); // This sends the connector object to Tableau
-        });
-    });
+	// Create event listeners for when the user submits the form
+	$(document).ready(function() {
+		$("#submitButton").click(function() {
+			tableau.connectionName = wdcName;
+			tableau.submit(); // This sends the connector object to Tableau
+		});
+	});
 })();
