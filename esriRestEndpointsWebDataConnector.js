5815219a5b045f6a85e044552c18e4614a42e053
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-05-16 15:33:32 -0500

    Delete esriRestEndpointsWebDataConnector.js

--- esriRestEndpointsWebDataConnector.js
+++
@@ -1,5 +0,0 @@
-const restApiUrls = [
-	{"common_name" : "The National Map", "url" : "https://cartowfs.nationalmap.gov/arcgis/rest"},
-	{"common_name" : "USDA", "url" : "https://gis.ers.usda.gov/arcgis/rest/"},
-	{"common_name" : "Franklin County, MO", "url" : "https://www.franklinmo.net/server/rest"}
-];
