9dc298df00b2aab181a329f1516fac6eb30d1020
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-05-16 09:46:20 -0500

    Update esriRestEndpointsWebDataConnector.js

--- esriRestEndpointsWebDataConnector.js
+++ esriRestEndpointsWebDataConnector.js
@@ -1,5 +1,5 @@
 const restApiUrls= {
-	"The National Map" : "https://cartowfs.nationalmap.gov/arcgis/rest",
-	"UNHCR" : "https://gis.unhcr.org/arcgis/rest",
-	"Franklin County, MO" : "https://www.franklinmo.net/server/rest",
-}
+	{"common_name" : "The National Map", "url" : "https://cartowfs.nationalmap.gov/arcgis/rest"},
+	{"common_name" : "UNHCR", "url" : "https://gis.unhcr.org/arcgis/rest"},
+	{"common_name" : "Franklin County, MO", "url" : "https://www.franklinmo.net/server/rest"}
+};
