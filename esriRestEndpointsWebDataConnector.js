f70a667a7b00620c7315589f0cb202d5fff8514c
AHamptonGA <88728390+AHamptonGA@users.noreply.github.com>
2022-05-16 09:36:57 -0500

    Add files via upload

---
+++ esriRestEndpointsWebDataConnector.js
@@ -0,0 +1,5 @@
+const restApiUrls= {
+	"The National Map" : "https://cartowfs.nationalmap.gov/arcgis/rest",
+	"UNHCR" : "https://gis.unhcr.org/arcgis/rest",
+	"Franklin County, MO" : "https://www.franklinmo.net/server/rest",
+}
